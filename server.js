79f7de1faffcc802475e179bbb873eb9c2fe5578
ritz3910@gmail.com <reetika.ojha@nagarro.com>
2018-03-12 10:29:41 +0530

    changes

--- server.js
+++ server.js
@@ -28,15 +28,7 @@ server.listen(port, function() {
 });
 
 server.get('/webhook/', function (req, res) {
-  let messaging_events = req.body.entry[0].messaging
-  for (let i = 0; i < messaging_events.length; i++) {
-    let event = req.body.entry[0].messaging[i]
-    let sender = event.sender.id
-    if (event.message && event.message.text) {
-      let text = event.message.text
-      sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
-    }
-  }
+ 
   res.sendStatus(200)
 })
 
